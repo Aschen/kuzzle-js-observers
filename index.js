67a81fca18c66023bb78c0e353167af313d2bfd2
Aschen <adrien@maret.email>
2020-03-31 12:10:12 +0200

    remove debug

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 const ObserveController = require('./lib/ObserveController');
-const {BaseController} = require('kuzzle-sdk');
-
-console.log(ObserveController.prototype.__proto__.constructor.name)
 
 module.exports = ObserveController;
